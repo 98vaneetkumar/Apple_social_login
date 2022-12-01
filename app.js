df93a760397b7172346cccf21a1cdf29b6059b5b
98vaneetkumar <vaneet3157@gmail.com>
2022-12-01 10:38:34 +0530

    Apple social login

---
+++ app.js
@@ -0,0 +1,68 @@
+require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const passport = require('passport');
+const AppleStrategy = require('passport-apple');
+
+app.get("/", (req, res) => {
+    res.send("<a href=\"/login\">Sign in with Apple</a>");
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+});
+
+passport.use(new AppleStrategy({
+        clientID: "",
+        teamID: "",
+        callbackURL: "",
+        keyID: "",
+        privateKeyLocation: ""
+    }, function(req, accessToken, refreshToken, idToken, profile , cb) {
+        // Here, check if the idToken.sub exists in your database!
+    	if (req.body && req.body.user) {
+      		// Register your user here!
+		console.log(req.body.user);
+	}
+    	cb(null, idToken);
+    }));
+
+app.get("/login", passport.authenticate('apple'));
+app.post("/auth", function(req, res, next) {
+	passport.authenticate('apple', function(err, user, info) {
+		if (err) {
+			if (err == "AuthorizationError") {
+				res.send("Oops! Looks like you didn't allow the app to proceed. Please sign in again! <br /> \
+				<a href=\"/login\">Sign in with Apple</a>");
+			} else if (err == "TokenError") {
+				res.send("Oops! Couldn't get a valid token from Apple's servers! <br /> \
+				<a href=\"/login\">Sign in with Apple</a>");
+			} else {
+				res.send(err);
+			}
+		} else {
+			if (req.body.user) {
+				res.json({
+					user: req.body.user,
+					idToken: user
+				});
+			} else {
+				res.json(user);
+			}			
+		}
+	})(req, res, next);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`http://localhost:${process.env.PORT}`);
+  });
